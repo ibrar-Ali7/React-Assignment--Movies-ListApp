c3765d345e8752f0bc00e5b58b7cbffd9d92bb2e
ibrar-Ali7 <imabrar955@gmail.com>
2024-12-13 19:40:36 +0500

    "Movies App v4"

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -7,8 +7,10 @@ import axios from 'axios';
 
 
 export default function NavBar() {
+ 
   const [movies,setMovies]=useState([])
   const [searchQuery,setSearchQuery] =useState('')
+  const [sorBy,setSortBy] =useState('popularity.desc')
   const handleOnChange = (event) =>{
     setSearchQuery(event.target.value)
   }
@@ -25,7 +27,10 @@ export default function NavBar() {
     setMovies(response.data.results);
     console.log(response.data.results);
   }
-
+  
+  const handleSortChange = (event)=>{
+    setSortBy(event.target.value)
+  }
   return (
     <div>
     <h2>E-MOVIES</h2>
@@ -33,8 +38,22 @@ export default function NavBar() {
       <input type='text' placeholder='Search movie...' value={searchQuery} onChange={handleOnChange} className='input-field'></input>
       <button className="Search-button" onClick={handleSearchSubmit}><AiOutlineSearch /></button>  
     </div>
+    <div className='filters'>
     <label htmlFor="sort-by">Sort By:</label>
-    <select  id="sort-by" value='' onChange={handleOnChange}></select>
+    <select  id="sort-by" value={sorBy} onChange={handleSortChange}>
+    <option value="popularity.desc"> Popularity Descending</option>
+    <option value="popularity.asc"> Popularity Ascending</option>
+    <option value="vote_average.asc"> Vote Average Ascending</option>
+    <option value="vote_average.desc"> Vote Average Descending</option>
+    <option value="release_date.asc"> Popularity Ascending</option>
+    <option value="release_date.desc"> Popularity Descending</option>
+    </select>
+    <label htmlFor="genre">Genre:</label>
+    <select  id="genre" value={selectGenre} onChange={handleGenreSelect}>
+    <option value=""> All</option>
+    </select>
+    </div>
+
     </div> 
   )
 }
